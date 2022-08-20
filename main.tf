3e3d422b9e377f2a8e9859da90c03c2cbb2706a1
David <24884783+Queenzlander@users.noreply.github.com>
2022-08-20 13:45:59 +1000

    added varariables.tf file

--- main.tf
+++ main.tf
@@ -17,7 +17,6 @@ provider "azurerm" {
   client_id       = var.clientID
   client_secret   = var.clientSecret
   tenant_id       = var.tenantID
-
   features {}
 }
 
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "subscriptionID" {
+  type = string
+}
+
+variable "clientID" {
+  type = string
+}
+
+variable "clientSecret" {
+  type = string
+}
+
+variable "tenantID" {
+  type = string
+}
+
+variable "rg_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
